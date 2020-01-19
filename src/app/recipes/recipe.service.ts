181b304c4a43c5aba75a056d44862d014d50c3c3
andreip <andrei.popescu@infoworld.ro>
2020-01-19 17:14:16 +0200

    Add the ingredients in the recipe model

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,28 @@
 import {Recipe} from './recipe.model';
 import {EventEmitter} from '@angular/core';
+import {Ingredient} from '../shared/ingredient.model';
 
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
-    new Recipe('A test Recipe', 'This is dummy', 'https://storage.needpix.com/rsynced_images/fish-1101436_1280.jpg'),
-    new Recipe('Another test Recipe', 'This is dummy', 'https://storage.needpix.com/rsynced_images/fish-1101436_1280.jpg')
+    new Recipe(
+      'Shaorma',
+      'This is yummeh',
+      'https://storage.needpix.com/rsynced_images/fish-1101436_1280.jpg',
+      [
+        new Ingredient('Carne', 1),
+        new Ingredient('Cartofi', 50),
+        new Ingredient('Cu de toate', 1)
+      ]),
+    new Recipe(
+      'Pui cu varza',
+      'Facuta de mata mare',
+      'https://storage.needpix.com/rsynced_images/fish-1101436_1280.jpg',
+      [
+        new Ingredient('Pui', 1),
+        new Ingredient('Varza', 1)
+      ])
   ];
 
   getRecipes() {
